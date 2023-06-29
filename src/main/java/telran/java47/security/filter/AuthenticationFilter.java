32e7356cdaec620c97f865d8c4ad33f9ce9c60d2
pavel <pavel@LAPTOP-JIUFII1O.mshome.net>
2023-06-29 12:37:08 +0300

    Security Context

--- src/main/java/telran/java47/security/filter/AuthenticationFilter.java
+++ src/main/java/telran/java47/security/filter/AuthenticationFilter.java
@@ -21,45 +21,52 @@ import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
 import telran.java47.accounting.dao.UserAccountRepository;
 import telran.java47.accounting.model.UserAccount;
+import telran.java47.security.context.SecurityContext;
 import telran.java47.security.model.User;
 
 @Component
-@Order(10)
 @RequiredArgsConstructor
+@Order(10)
 public class AuthenticationFilter implements Filter {
 
 	final UserAccountRepository userAccountRepository;
+	final SecurityContext securityContext;
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) resp;
-		
-		System.out.println(request.getSession().getId());
-		
 		if (checkEndPoint(request.getMethod(), request.getServletPath())) {
-			String[] credentials;
-			try {
-				credentials = getCredentials(request.getHeader("Authorization"));
-			} catch (Exception e) {
-				response.sendError(401, "token is not valid");
-				return;
+			String sessionId = request.getSession().getId();
+			User user = securityContext.getUserBySessionId(sessionId);
+			if(user == null) {
+				String[] credentials;
+				try {
+					credentials = getCredentials(request.getHeader("Authorization"));
+				} catch (Exception e) {
+					response.sendError(401, "token not valid");
+					return;
+				}
+				UserAccount userAccount = userAccountRepository.findById(credentials[0]).orElse(null);
+				if (userAccount == null || !BCrypt.checkpw(credentials[1], userAccount.getPassword())) {
+					response.sendError(401, "login or password is not valid");
+					return;
+				}
+				user = new User(userAccount.getLogin(), userAccount.getRoles());
+				securityContext.addUserSession(sessionId, user);
 			}
-			UserAccount userAccount = userAccountRepository.findById(credentials[0]).orElse(null);
-			if (userAccount == null || !BCrypt.checkpw(credentials[1], userAccount.getPassword())) {
-				response.sendError(401, "login or password is not valid");
-				return;
-			}
-			request = new WrappedRequest(request, userAccount.getLogin(),userAccount.getRoles());
+			
+			request = new WrappedRequest(request, user.getName(), user.getRoles());
 		}
 		chain.doFilter(request, response);
-
 	}
 
 	private boolean checkEndPoint(String method, String path) {
-		return !(("POST".equalsIgnoreCase(method) && path.matches("/account/register/?"))
-				|| path.matches("/forum/posts/\\w+(/\\w+)?/?"));
+		return !(
+				("POST".equalsIgnoreCase(method) && path.matches("/account/register/?"))
+				|| path.matches("/forum/posts/\\w+(/\\w+)?/?")
+				);
 	}
 
 	private String[] getCredentials(String token) {
@@ -77,10 +84,10 @@ public class AuthenticationFilter implements Filter {
 			this.login = login;
 			this.roles = roles;
 		}
-
+		
 		@Override
 		public Principal getUserPrincipal() {
-			return new User(login,roles);
+			return new User(login, roles);
 		}
 
 	}
